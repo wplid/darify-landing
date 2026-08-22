8f12429e14eda62cc8579f507719c6120551a1a3
wplid <walidkadous22@gmail.com>
2026-08-22 20:26:46 +0100

    Update Facebook URL

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 const SOCIALS = [
   {
     name: "Facebook",
-    href: "https://www.facebook.com/61592801965174/",
+    href: "https://www.facebook.com/darifyapp",
     icon: (
       <svg viewBox="0 0 24 24" fill="currentColor" className="h-4 w-4">
         <path d="M22 12a10 10 0 1 0-11.6 9.9v-7H7.9V12h2.5V9.8c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.5h-1.2c-1.2 0-1.6.8-1.6 1.6V12h2.7l-.4 2.9h-2.3v7A10 10 0 0 0 22 12z" />
